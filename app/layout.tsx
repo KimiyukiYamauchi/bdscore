7110ee7aa8b7bf19051f95c901e72975e8d6d533
Kimiyuki Yamauchi <kimiyuki.yamauchi.0313@gmail.com>
2025-11-28 17:47:21 +0900

    titleとfavicon.icoを修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Badminton Scorekeeper",
+  title: "BadScore",
   description: "A simple badminton scorekeeping web app",
 };
 
